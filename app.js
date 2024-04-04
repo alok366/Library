51a298f9e5da73dbc128efb1167fa59c445ad856
alok366 <91714340+alok366@users.noreply.github.com>
2024-04-04 12:00:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const dev_db_url = "mongodb+srv://alok:Mqu81xWpeIgOuPVi@cluster0.lnokgep.mongodb.net/?retryWrites=true&w=majority";
+const dev_db_url = "add your mongo url";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
